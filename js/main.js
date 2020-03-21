09a7e47804aaac452d27ed149508193c535775ee
Ajeet yadav <ajeetyadav164@gmail.com>
2020-03-21 12:48:16 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
-
-
-
-
-    
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
+    new GitHubCalendar("#github-graph", "ajeetAk47");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "ajeetAk47", selector: "#ghfeed" });
