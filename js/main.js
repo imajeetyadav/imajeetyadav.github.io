ee0413d67e04d3b9f429329279301ea924bae30a
Ajeet yadav <ajeetyadav164@gmail.com>
2020-04-09 12:35:59 +0530

    portfolio new Design

--- js/main.js
+++
@@ -1,6 +0,0 @@
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "ajeetAk47");
-    
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "ajeetAk47", selector: "#ghfeed" });
